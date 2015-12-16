9ea98b6a465667139b192a159c835351a311f4e1
RikkaW <s574204775@gmail.com>
2015-12-16 16:28:16 +0800

    无聊的东西...

--- app/src/main/java/rikka/searchbyimage/MainActivity.java
+++ app/src/main/java/rikka/searchbyimage/MainActivity.java
@@ -2,12 +2,18 @@ package rikka.searchbyimage;
 
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.SwitchPreferenceCompat;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,9 +29,13 @@ public class MainActivity extends AppCompatActivity {
                 new SettingsFragment()).commit();
     }
 
-    public static class SettingsFragment extends PreferenceFragmentCompat implements OnSharedPreferenceChangeListener {
+    public static class SettingsFragment extends PreferenceFragmentCompat implements
+            OnSharedPreferenceChangeListener,
+            Preference.OnPreferenceClickListener {
+
         PreferenceCategory mCategoryGeneral;
         SwitchPreferenceCompat mSafeSearch;
+
         @Override
         public void onCreatePreferences(Bundle bundle, String s) {
             addPreferencesFromResource(R.xml.preferences);
@@ -34,6 +44,15 @@ public class MainActivity extends AppCompatActivity {
             mSafeSearch = (SwitchPreferenceCompat) findPreference("safe_search_preference");
 
             setSafeSearchHide();
+
+            Preference versionPref = findPreference("version");
+            versionPref.setOnPreferenceClickListener(this);
+
+            try {
+                versionPref.setSummary(getContext().getPackageManager().getPackageInfo(getContext().getPackageName(), 0).versionName);
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
         }
 
         @Override
@@ -67,6 +86,32 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 mCategoryGeneral.removePreference(mSafeSearch);
             }
+        }
+
+        private int click = 0;
+
+        private Runnable clearClickCount = new Runnable() {
+            @Override
+            public void run() {
+                click = 0;
+            }
+        };
+
+        @Override
+        public boolean onPreferenceClick(Preference preference) {
+            getActivity().getWindow().getDecorView().removeCallbacks(clearClickCount);
+            getActivity().getWindow().getDecorView().postDelayed(clearClickCount, 3000);
+
+            click ++;
+
+            if (click == 5)
+                Toast.makeText(getContext(), "OAO", Toast.LENGTH_LONG).show();
+            else if (click == 10)
+                Toast.makeText(getContext(), "><", Toast.LENGTH_LONG).show();
+            else if (click == 25)
+                Toast.makeText(getContext(), "QAQ", Toast.LENGTH_LONG).show();
+
+            return false;
         }
     }
 }
