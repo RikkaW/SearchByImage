aeada80e40dc04d90773888d49452def9353c831
qixingchen <qixingchen@lovelive.moe>
2015-12-17 23:49:34 +0800

    更换 preference-v7 为 preference-v14

--- app/src/main/java/rikka/searchbyimage/widget/ListPreference2.java
+++ app/src/main/java/rikka/searchbyimage/widget/ListPreference2.java
@@ -3,7 +3,7 @@ package rikka.searchbyimage.widget;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
-import android.preference.ListPreference;
+import android.support.v7.preference.ListPreference;
 import android.util.AttributeSet;
 
 /**
