9fdfb7977e2d116dce1b2f6ff004b1ac0ddaa85f
Rikka <rikka@xing.moe>
2016-04-17 13:25:26 +0800

    remove leancloud SDK for play version

---
+++ app/src/play/java/rikka/searchbyimage/SearchByImageApplication.java
@@ -0,0 +1,36 @@
+package rikka.searchbyimage;
+
+import android.app.Application;
+
+import java.io.InputStream;
+
+import moe.xing.daynightmode.DayNightMode;
+import rikka.searchbyimage.support.CrashHandler;
+
+/**
+ * Created by Rikka on 2015/12/31.
+ */
+public class SearchByImageApplication extends Application {
+    // TODO: change this to save to file
+    private InputStream imageInputStream;
+
+    public InputStream getImageInputStream() {
+        return imageInputStream;
+    }
+
+    public void setImageInputStream(InputStream imageInputStream) {
+        this.imageInputStream = imageInputStream;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        DayNightMode.setDefaultNightMode(DayNightMode.MODE_NIGHT_AUTO);
+
+        if (!BuildConfig.hideOtherEngine && !BuildConfig.DEBUG) {
+            CrashHandler.init(getApplicationContext());
+            CrashHandler.register();
+        }
+    }
+}
